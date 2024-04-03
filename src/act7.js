c49ec9e6a944595d210b0c754b5e9557236b0131
BilalBoulouban <bilal.boulouban@cirvianum.cat>
2024-04-03 17:59:47 +0200

    bueno img ni

--- src/act7.js
+++ src/act7.js
@@ -11,14 +11,13 @@ async function setMovieHeading(
   const title = document.querySelector(titleSelector);
   const info = document.querySelector(infoSelector);
   const director = document.querySelector(directorSelector);
-  
-if (!movieId) {
-  title.innerHTML='';
-  info.innerHTML= '';
-  director.innerHTML= '';
-  return;
-  
-}
+
+  if (!movieId) {
+    title.innerHTML = "";
+    info.innerHTML = "";
+    director.innerHTML = "";
+    return;
+  }
   // Obtenim la informació de la pelicula
   const movieInfo = await swapi.getMovieInfo(movieId);
   // Injectem
@@ -56,33 +55,90 @@ async function initMovieSelect(selector) {
 
 // EVENT HANDLERS //
 
-function addChangeEventToSelectHomeworld() {}
+function addChangeEventToSelectHomeworld() {
+  document.querySelector(".list_characters").innerHTML = "";
+  const homeworld = document.querySelector("#select_homeworld");
+  homeworld.addEventListener("change", _createCharacterTokens);
+}
 
-async function _createCharacterTokens() {}
+//event handlers
 
-function _addDivChild(parent, className, html) {}
+async function _createCharacterTokens(event) {
+  //Necessitem saber quina pelicula i quin planeta homeworls shan seleccionat
+  const idPelicula = document.querySelector("#select_movies").value;
+  const selectHomeworld = document.querySelector("#select_homeworld").value;
+  const ul = document.querySelector(`.list_characters`);
+  if (!idPelicula) {
+    throw Error("no movie selected.");
+  }
+  // if(!selectHomeworld){
+  //   throw Error('No homeworld selected')
+  // }
+  //
+  let movieInfo = await swapi.getMovieCharactersAndHomeworlds(idPelicula);
+
+  let filteredcaracters = movieInfo.filter(
+    (caracter) => caracter.homeword == event.target.value
+  );
+  console.log(filteredcaracters);
+
+  filteredcaracters.map((personatge) => {
+    const li = document.createElement("li");
+    li.classList.add("list_item", "item", "character");
+    ul.appendChild(li);
+    //les imatges les tenim en local. hem de crear un element img
+    const img = document.createElement("img");
+    //anem a introduir la imatge del personatge
+    const urlParts = personatge.url.split("/");
+    const characterNumber = urlParts[urlParts.length - 1];
+    //const characterNumber = urlPart.pop()
+    img.src = `../public/assets/people/${characterNumber}.jpg`;
+    img.className = "character_image";
+    img.style.maxWidth = "100%";
+    li.appendChild(img);
+    // em de afeguir el nom del personatge
+    const h2 = document.createElement("h2");
+    h2.classList.add("character_name");
+    h2.innerHTML = personatge.name;
+    li.appendChild(h2);
+    _addDivChild(li, 'character__birth','<strong>Birth Year:</strong>' + personatge.birth_year);
+    _addDivChild(li, 'character__eye','<strong>Eye Color:</strong>' + personatge.eye_color);
+    _addDivChild(li, 'character__gender','<strong>Gender:</strong>' + personatge.gender);
+    _addDivChild(li, 'character__home','<strong>Homeworld:</strong>' + personatge.homeword);
+
+  });
+
+  
+}
+
+function _addDivChild(parent, className, html) {
+  const div = document.createElement('div')
+  div.className = className
+  div.innerHTML = html 
+  parent.appendChild(div)
+}
 
 function setMovieSelectCallbacks() {
-  const selectMovie = document.querySelector('#select-movie');
-  selectMovie.addEventListener('change'  , _handleOnSelectMovieChanged) 
+  const selectMovie = document.querySelector("#select-movie");
+  selectMovie.addEventListener("change", _handleOnSelectMovieChanged);
 }
 
 async function _handleOnSelectMovieChanged(event) {
   //obtenir  el id de la pel·licula seleccionada
-  const movieID = event.target.value
+  const movieID = event.target.value;
   //modifiquem la capsalera amb la informacio correcponent mab aquesta peli
- setMovieHeading(movieID, '.movie__title', '.movie__info', '.movie__director');
+  setMovieHeading(movieID, ".movie__title", ".movie__info", ".movie__director");
 
- //ex4 recuperar infomracio de l'auxiliar _populateHomeWorldSelector
- const selector = document.querySelector('#select-homeworld');
+  //ex4 recuperar infomracio de l'auxiliar _populateHomeWorldSelector
+  const selector = document.querySelector("#select-homeworld");
 
- const caracters = await swapi.getMovieCharactersAndHomeworlds(movieID);
- //necesitem una llista amb els planetes d'origuen dels diferents personatges
- const homeworlds = caracters.characters.map((caracter) => caracter.homeworld);
- const homeworldsFiltred = _removeDuplicatesAndSort(homeworlds);
- //amb la llista neta i de fet ordenada podem cridad a la funcio que actualitza el selector de homeword
- _populateHomeWorldSelector(homeworldsFiltred,selector);
- document.querySelector('.list_caracters').innerHTML = '';
+  const caracters = await swapi.getMovieCharactersAndHomeworlds(movieID);
+  //necesitem una llista amb els planetes d'origuen dels diferents personatges
+  const homeworlds = caracters.characters.map((caracter) => caracter.homeworld);
+  const homeworldsFiltred = _removeDuplicatesAndSort(homeworlds);
+  //amb la llista neta i de fet ordenada podem cridad a la funcio que actualitza el selector de homeword
+  _populateHomeWorldSelector(homeworldsFiltred, selector);
+  document.querySelector(".list_caracters").innerHTML = "";
 }
 
 function _filmIdToEpisodeId(episodeID) {
@@ -114,21 +170,19 @@ function _setMovieHeading({ name, episodeID, release, director }) {}
 
 function _populateHomeWorldSelector(homeworlds) {
   console.log(homeworlds);
-  const selector = document.querySelector('#select-homeworld');
-  selector.innerHTML= '';
+  const selector = document.querySelector("#select-homeworld");
+  selector.innerHTML = "";
   //aqui implementem la logica per poblar o injectar els planetes al desplegable homeworld
-  const option = document.createElement('option');
-  option.value= '';
-  option.textContent='Selecciona un homeworld';
+  const option = document.createElement("option");
+  option.value = "";
+  option.textContent = "Selecciona un homeworld";
   selector.appendChild(option);
   homeworlds.forEach((homeword) => {
-    const option =document.createElement('option');
-    option.value= homeword;
-    option.textContent= homeword;
+    const option = document.createElement("option");
+    option.value = homeword;
+    option.textContent = homeword;
     selector.appendChild(option);
   });
-
-
 }
 
 /**
